1e0c3a0b0b2959254764bec7e6fc6637c219548c
Rajender Kumar <ft.rajender@gmail.com>
2023-03-15 18:41:08 +0700

    fix Bug

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,7 +12,7 @@ const Form = () => {
             <input className='w-full border-2 p-2.5 my-2 rounded-md shadow-inner shadow-gray-800 outline-none  ' type="Name" name="name" placeholder='Name'/>
             <input className='w-full border-2 p-2.5 my-2 rounded-md shadow-inner shadow-gray-800 outline-none  ' type="Email" name="email" placeholder='Email' />
             <textarea className='w-full border-2 p-2.5 my-2 rounded-md shadow-inner shadow-gray-800 outline-none ' name="message" id="" cols="30" rows="4" placeholder='Message'></textarea>
-            <button className='w-36 h-12 bg-sky-500 mx-auto my-4 lg:my-6 rounded-md lg:animate-bounce duration-300 lg:hover:animate-none hover:bg-sky-400'>Lets Collaborate</button>
+            <button className='w-36 h-12 bg-sky-500 mx-auto my-4 lg:my-6 rounded-md lg:animate-bounce duration-300 lg:hover:animate-none hover:bg-sky-400 shadow-md shadow-gray-400'>Lets Collaborate</button>
         </form>
     </section>
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,14 @@ import resume from '../Assets/Rajender-Kumar-resume.pdf'
 const Header = () => {
   return (
     <>
-      <header name="home" className="w-full h-screen bg-black/50 ">
+      <header name="home" className="w-full h-full bg-black/50 ">
         {/* VIDEO */}
         <video className="object-cover w-full h-screen mix-blend-overlay" src={video} autoPlay muted loop />
         {/* TEXT */}
-        <div className="absolute w-96 h-auto sm:w-[37rem] lg:w-[45rem] text-white top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] space-y-2">
+        <div className="p-1 absolute w-72 h-auto sm:w-[37rem] lg:w-[45rem] text-white top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] space-y-2">
           <p className="text-sm sm:text-lg sm:font-medium">Hii My Name is</p>
-          <h1 className="text-3xl sm:text-5xl lg:text-6xl uppercase font-medium"> <p className="text-sky-500"> Rajender Kumar</p></h1>
-          <p className="text-3xl sm:text-5xl lg:text-6xl font-semibold text-gray-200">I'm a Front-End Developer</p>
+          <h1 className="text-2xl sm:text-5xl lg:text-6xl uppercase font-medium"> <p className="text-sky-500"> Rajender Kumar</p></h1>
+          <p className="text-2xl sm:text-5xl lg:text-6xl font-medium sm:font-semibold text-gray-200">I'm a Front-End Developer</p>
           <p className="text-gray-400 text-sm sm:text-base pb-7"> I have a strong passion for creating websites and web applications that are visually appealing and highly functional. </p>
           <a className=' w-25 text-gray-300  border-2 p-2 hover:bg-sky-400 hover:border-sky-400 hover:text-black duration-300' download={true} href={resume}>Download Resume</a>
         </div>
